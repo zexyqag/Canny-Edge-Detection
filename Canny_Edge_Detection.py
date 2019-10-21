789bf535a91d93cc757d54f9976df86cb7861869
Martin <zexyqag@gmail.com>
2019-10-21 22:39:12 +0200

    Made shit awesome n stuff; why are you reading this

--- Canny_Edge_Detection.py
+++ Canny_Edge_Detection.py
@@ -1,95 +1,71 @@
 import cv2
 import math
 import multiprocessing
+from enum import Enum
 
-image = cv2.imread('image.jpg')
+class EdgeHandling(Enum):
+    extend = 1
+    wrap = 2
+    mirror = 3
+    crop = 4
+    kernelCrop = 4
+
+class KernelType(Enum):
+    pascal = 1
+    mean = 2
 
 def GrayScale(img):
     img = img.copy()
     for x, row in enumerate(img):
         for y, pixel in enumerate(row):
-            intensity = int(sum(img[x][y])/3)
+            intensity = int(sum(img[x][y]) / 3)
             for z, subpixel in enumerate(pixel):
                 img[x][y][z] = intensity
     return img
 
-def meanBlur(img, size):
-    blurImg = img.copy()
-    size2 = (size*2)-1
-    for x, row in enumerate(img):
-        for y, pixel in enumerate(row):
-            for z, subpixel in enumerate(pixel):
-                avrageVal = 0
+def Convolution(image, kernel, edgeHandling):
+    modImg = image.copy()
+    kernelRadius = int(math.floor(len(kernel) / 2))
+
+    for j, row in enumerate(image):
+        for k, pixel in enumerate(row):
+            for l, subpixel in enumerate(pixel):
+                accumulator = 0
                 cals = 0
-                for i in range(0, size2**2):
-                    try:
-                        avrageVal += int(img[x+i%size2-size+1][y+math.floor(i/size2)-size+1][z])
-                        cals += 1
-                    except IndexError:
-                        pass
-                    continue
-                avrageVal = avrageVal / cals
-                blurImg[x][y][z] = avrageVal
-    return blurImg
-
-def gaussianBlurPascal(img, size):
-    blurImg = img.copy()
-    size2 = (size*2)-1
-    stddiv = pascal(size2)
-
-    for x, row in enumerate(img):
-        for y, pixel in enumerate(row):
-            for z, subpixel in enumerate(pixel):
-                avrageVal = 0
-                cals = 0
-                for i in range(0, size2**2):
-                    xi = i%size2
-                    yi = math.floor(i/size2)
-                    weight = stddiv[xi] * stddiv[yi]
-                    try:
-                        avrageVal += int(img[x+xi-size+1][y+yi-size+1][z])*weight
-                        cals += weight
-                    except IndexError:
-                        pass
-                    continue
-                avrageVal = avrageVal / cals
-                blurImg[x][y][z] = avrageVal
-    return blurImg
-
-def edgeDetectionVector():
-    array = [0]
-    return array
-
-def edgeDetectionColor():
-    array = [0]
-    return array
-
-def edgeDetectionGrey():
-    array = [0]
-    return array
+                for m, kernelRow in enumerate(kernel):
+                    for n, element in enumerate(kernelRow):
+                        mi = j + m - kernelRadius
+                        ni = k + n - kernelRadius
+                        if (mi >= 0 <= ni and mi < len(image) and ni < len(image[j]) and element != 0):
+                            accumulator += int(image[mi][ni][l]) * element
+                            cals += element
+                modImg[j][k][l] = accumulator / cals
+    return modImg
 
 
-def pascal(n):
-  Row = [1]
-  for i in range(n):
-    Row.append(Row[i] * (n-i) / (i+1))
-  return Row
+def KernelGenerator(size, kernelType):
+    if (kernelType == KernelType.pascal):
+        size -= 1
+        row = [1]
+        for i in range(size):
+            row.append(row[i] * (size - i) / (i + 1))
+        return MetrixFromRow(row)
+    elif (kernelType == KernelType.mean):
+        return MetrixFromRow([1 for x in range(size)])
+        
 
-
-
+def MetrixFromRow(row):
+    metrix = [[0 for x in range(len(row))] for y in range(len(row))]
+    for i, metrixRow in enumerate(metrix):
+        for j, element in enumerate(metrixRow):
+            metrix[i][j] = row[j] * row[i]
+    return metrix
 
 
 if __name__ == '__main__':
-    #p = Process(target=f, args=('bob',))
-    #p.start()
-    #p.join()
-    image = GrayScale(image)
-
-    #cv2.imshow('image0', image)
-    #cv2.imshow('image1', meanBlur(image, 3))
-    cv2.imshow('image2', gaussianBlurPascal(image, 3))
-
+    image = cv2.imread('image.jpg')
+    cv2.imshow('image0', image)
+    cv2.imshow('image1', Convolution(image, KernelGenerator(7,KernelType.pascal), EdgeHandling.kernelCrop))
+    cv2.imshow('image2', Convolution(image, KernelGenerator(7,KernelType.mean), EdgeHandling.kernelCrop))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
-
