c56b0b4a56250fbe22ed16d9b43a83b676cd8c9e
Martin <zexyqag@gmail.com>
2019-10-28 01:37:21 +0100

    methods works like a charm, set main up to do diagonal edge detection

--- Canny_Edge_Detection.py
+++ Canny_Edge_Detection.py
@@ -1,20 +1,19 @@
 import cv2
-import math
 import multiprocessing
+import numpy as np
 from enum import Enum
-
-class EdgeHandling(Enum):
-    extend = 1
-    wrap = 2
-    mirror = 3
-    crop = 4
-    kernelCrop = 4
 
 class KernelType(Enum):
     pascal = 1
     mean = 2
     sobely = 3
     sobelx = 4
+    prewitty = 5
+    prewittx = 6
+    emboss = 7
+    outline = 8
+    sobely45 = 9
+    sobelx45 = 10
 
 def GrayScale(img):
     img = img.copy()
@@ -25,26 +24,28 @@ def GrayScale(img):
                 img[x][y][z] = intensity
     return img
 
-def Convolution(image, kernel, edgeHandling):
-    modImg = image.copy()
-    kernelRadius = int(math.floor(len(kernel) / 2))
+def Convolution(image, kernel, normalization):
+    modImg = np.array(image.copy(), dtype = int)
+    kernelRadius = len(kernel) // 2
 
     for j, row in enumerate(image):
         for k, pixel in enumerate(row):
             for l, subpixel in enumerate(pixel):
                 accumulator = 0
-                cals = 0
+                weightSum = 0
                 for m, kernelRow in enumerate(kernel):
                     for n, element in enumerate(kernelRow):
                         mi = j + m - kernelRadius
                         ni = k + n - kernelRadius
                         if (mi >= 0 <= ni and mi < len(image) and ni < len(image[j]) and element != 0):
                             accumulator += int(image[mi][ni][l]) * element
-                            cals += abs(element)
-                modImg[j][k][l] = accumulator / cals
-    return modImg
+                            weightSum += abs(element)
+                modImg[j][k][l] = (accumulator / weightSum if normalization else accumulator)
+    return (np.array(modImg.copy(), dtype = np.uint8) if normalization else modImg)
 
 def KernelGenerator(*args):
+
+    #arguments handling
     if (len(args) == 0):
         raise Exception('Missing first argument of type KernelType')
     elif (len(args) >= 3):
@@ -62,6 +63,7 @@ def KernelGenerator(*args):
         elif (type(kernelType) != KernelType):
             raise Exception('First arguemnt needs to be of type KernelType')
         
+        #Kernel handling
     if (kernelType == KernelType.pascal):
         size -= 1
         row = [1]
@@ -70,16 +72,39 @@ def KernelGenerator(*args):
         return MetrixFromRow(row)
     elif (kernelType == KernelType.mean):
         return MetrixFromRow([1 for x in range(size)])
-    elif (kernelType == KernelType.sobelx):
-        return [[1,0,-1],
-                [2,0,-2],
-                [1,0,-1]]
     elif (kernelType == KernelType.sobely):
-        return [[1, 2, 1],
-                [0, 0, 0],
-                [-1,-2,-1]]
+        return [[-1, 0, 1],
+                [-2, 0, 2],
+                [-1, 0, 1]]
+    elif (kernelType == KernelType.sobelx):
+        return [[-1,-2,-1],
+                [ 0, 0, 0],
+                [ 1, 2, 1]]
+    elif (kernelType == KernelType.prewitty):
+        return [[-1, 0, 1],
+                [-1, 0, 1],
+                [-1, 0, 1]]
+    elif (kernelType == KernelType.prewittx):
+        return [[-1,-1,-1],
+                [ 0, 0, 0],
+                [ 1, 1, 1]]
+    elif (kernelType == KernelType.emboss):
+        return [[-2,-1, 0],
+                [-1, 1, 1],
+                [ 0, 1, 2]]
+    elif (kernelType == KernelType.outline):
+        return [[-1,-1,-1],
+                [-1, 8,-1],
+                [-1,-1,-1]]
+    elif (kernelType == KernelType.sobelx45):
+        return [[ 0, 1, 2],
+                [-1, 0, 1],
+                [-2,-1, 0]]
+    elif (kernelType == KernelType.sobely45):
+        return [[-2,-1, 0],
+                [-1, 0, 1],
+                [ 0, 1, 2]]
         
-
 def MetrixFromRow(row):
     metrix = [[0 for x in range(len(row))] for y in range(len(row))]
     for i, metrixRow in enumerate(metrix):
@@ -87,16 +112,48 @@ def MetrixFromRow(row):
             metrix[i][j] = row[j] * row[i]
     return metrix
 
+def GradientMagnitude(imagex, imagey):
+    modImgx = np.array(imagex.copy(), dtype = np.int)
+    modImgy = np.array(imagey.copy(), dtype = np.int)   
+    return np.sqrt(modImgx ** 2 + modImgy ** 2)
+
+def ImageNormalization(image):
+    modImg = image.copy()
+    modImg = modImg + np.absolute(np.amin(modImg))
+    modImg = modImg * (255 / np.amax(modImg))
+    return np.array(modImg, dtype = np.uint8)
+
+def ImageThresholding(image, threshold):
+    modImg = image.copy()
+    modImg[modImg >= threshold] = 255
+    modImg[modImg < threshold] = 0
+    return modImg
+
 
 if __name__ == '__main__':
+    #Get image
     image = cv2.imread('image2.jpg')
-    cv2.imshow('image0', image)
-    image = GrayScale(image)
-    cv2.imshow('image1', image)
-    image = Convolution(image, KernelGenerator(KernelType.pascal, 11), EdgeHandling.kernelCrop)
-    cv2.imshow('image2', image)
-    image = Convolution(image, KernelGenerator(KernelType.sobelx), EdgeHandling.kernelCrop)
-    cv2.imshow('image3', image)
+    cv2.imshow('image', image)
+
+    #Gray scale image
+    imageGray = GrayScale(image)
+    cv2.imshow('imageGray', imageGray)
+
+    #Blur image
+    imageBlured = Convolution(imageGray, KernelGenerator(KernelType.pascal, 11), True)
+    cv2.imshow('imageBlured', imageBlured)
+
+    #Detect edges image
+    imageEdge = Convolution(imageBlured, KernelGenerator(KernelType.sobelx45), False)
+    cv2.imshow('imageEdge', ImageNormalization(imageEdge))
+
+    #Get Magnetude
+    imageMagnetude = ImageNormalization(GradientMagnitude(imageEdge, imageEdge))
+    cv2.imshow('imageMagnetude', imageMagnetude)
+
+    #Threshold image
+    imageThreshold = ImageThresholding(imageMagnetude, 50)
+    cv2.imshow('imageThreshold', imageThreshold)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
